385d3f90817787fae39889e8f6f21c019abd2ea0
anurag9601 <anuragmishrap13@gmail.com>
2024-12-12 22:19:18 +0530

    update 12_dec.py

--- 12_dec.py
+++ 12_dec.py
@@ -41,6 +41,53 @@ def sort_dates(lst, mode):
 
 # print(sort_dates(["10-02-2018_12:30", "10-02-2016_12:30", "10-02-2018_12:15"], "DSC"))
 
+def time_adjust(string, hr, mint, sec):
+    format_time = []
+
+    props_lst = [hr, mint, sec]
+
+    for i in (string.split(":")):
+        format_time.append(int(i))
+
+    for j in range(len(props_lst)):
+        if(j == 0):
+            remain = props_lst[j]//24
+            if(remain == 0):
+                format_time[j] = format_time[j] + props_lst[j]
+        elif(j == 1):
+            remain = (props_lst[j] + format_time[j]) //60
+            if(remain > 0):
+                format_time[0] = format_time[0] + remain
+                format_time[j] = (props_lst[j] + format_time[j]) % 60
+        elif(j == 2):
+            remain = (props_lst[j] + format_time[j]) // 60
+            if(remain > 0):
+                format_time[1] = format_time[1] + remain
+                format_time[j] = (props_lst[j] + format_time[j]) % 60
+    
+    result = ""
+    for i in range(len(format_time)):
+        if(format_time[i] == 0):
+            result += "00"
+            if(i != len(format_time)-1):
+                result += ":"
+        else:
+            str_num = str(format_time[i])
+            if(len(str_num) == 1):
+                str_num = "0" + str_num
+            result += str_num
+            if(i != len(format_time)-1):
+                result += ":"
+
+    return result
+
+# print(time_adjust("18:22:30", 4, 60, 30))
+
+print(time_adjust("00:00:00", 72, 120, 120))
+
+
+
+
 
 
 
