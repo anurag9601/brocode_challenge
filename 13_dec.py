da6c81489c1c317d5c869f2562853a96bf2e28fa
anurag9601 <anuragmishrap13@gmail.com>
2024-12-13 23:37:54 +0530

    update 13_dec.py

--- 13_dec.py
+++ 13_dec.py
@@ -43,4 +43,44 @@ def two_sum(numbers, target):
             right += 1
 
 # print(two_sum([2, 7, 11, 15], 9))
+# print(two_sum([1, 3, 5, 7, 9], 12))
 # print(two_sum(list(range(1, 10001)), 19999))
+
+def find_long_substr(input_str):
+    longest_substr = ""
+
+    if(len(input_str) == 1):
+        return input_str
+    else:
+        for i in range(len(input_str)):
+            temp = ""
+            for j in range(i, len(input_str)):
+                if(input_str[j] not in temp):
+                    temp += input_str[j]
+                    if(len(temp) > len(longest_substr)):
+                        longest_substr = temp
+                else:
+                    break
+    return len(longest_substr)
+
+# print(find_long_substr("abc def ghi"))
+# print(find_long_substr("abcdabcdabcdabcd"))
+
+#Return an array where each element is the product of all elements except itself.
+
+def array_accept_self(lst):
+    result = []
+
+    for i in range(len(lst)):
+        sum = 0
+        for j in range(len(lst)):
+            if(j != i):
+                if(sum == 0):
+                    sum = lst[j]
+                else:
+                    sum *= lst[j]
+        result.append(sum)
+    return result
+
+# print(array_accept_self([1, 2, 3, 4]))
+
