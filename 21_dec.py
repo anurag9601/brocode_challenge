49e0106422877c1656a4f5f3dfc4687691a22e88
anurag9601 <anuragmishrap13@gmail.com>
2024-12-22 12:51:13 +0530

    update 21_dec.py

--- 21_dec.py
+++ 21_dec.py
@@ -24,3 +24,35 @@ def reverse_integer(int):
 
 # print(reverse_integer(120))
 # print(reverse_integer(-123))
+
+def time_around_word(city_a, timestamp, city_b):
+    adding_time = "00:00"
+
+    split_timestamp = timestamp.split(" ")
+    time_split = split_timestamp[3].split(":")
+
+    year = split_timestamp[2]
+    month = split_timestamp[0]
+    day = split_timestamp[1][:-1]
+    hour = time_split[0]
+    minute = time_split[1]
+
+    cities_time_gap = {
+        "Los Angeles" : "-08:00",
+        "New York" : "-05:00",
+        "Caracas" : "-04:30",
+        "Buenos Aires" : "-03:00",
+        "London" : "+00:00",
+        "Rome" : "+01:00",
+        "Moscow" : "+03:00",
+        "Tehran" : "+03:30",
+        "New Delhi" : "+05:30",
+        "Beijing" :"+08:00",
+        "Canberra" : "+10:00",
+    }
+
+    city_a_time_gap = cities_time_gap[city_a]
+    city_b_time_gap = cities_time_gap[city_b]
+
+time_around_word("Los Angeles", "April 1, 2011 23:23", "Canberra")
+    
