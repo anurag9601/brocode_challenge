84ad3d00956ab84013f8aac546e607d5e1a89a48
anurag9601 <anuragmishrap13@gmail.com>
2024-12-23 23:46:16 +0530

    update 23_dec.py

--- 23_dec.py
+++ 23_dec.py
@@ -65,5 +65,104 @@ def num_split(n):
 # print(num_split(39))
 # print(num_split(1234))
 
+import time
+
+def tic_tac_toe():
+    board = [
+        ["1", "2", "3"],
+        ["4", "5", "6"],
+        ["7", "8", "9"]
+    ]
+
+    game_over = False
+
+    turn = "X"
+
+    def update_board(index):
+        nonlocal turn
+
+        try:
+            int(index)
+        except:
+            return "Invalid"
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if(board[i][j] == index):
+                    board[i][j] = turn
+                    check_win()
+                    turn = "O" if turn == "X" else "X"
+                    return "Valid"
+        return "Invalid"
+
+    def print_board():
+        for i in board:
+            print(i)
+        
+    def celebration(message, delay):
+        for char in message:
+            print(char, end="", flush=True)
+            time.sleep(delay)
+        print()
+
+    def check_win():
+        nonlocal game_over
+
+        for i in range(len(board)):
+            if(board[i][0] == board[i][1] and board[i][1] == board[i][2]):
+                game_over = True
+                celebration_message = f"{turn} Won🥳 AAHA TAMATAR BADE MAJEDAAR..."
+                celebration(celebration_message, 0.2)
+                return
+
+        for j in range(len(board[0])):
+            if(board[0][j] == board[1][j] and board[1][j] == board[2][j]):
+                game_over = True
+                celebration_message = f"{turn} Won🥳 AAHA TAMATAR BADE MAJEDAAR..."
+                celebration(celebration_message, 0.2)
+                return
+
+        if(board[0][0] == board[1][1] and board[1][1] == board[2][2]):
+            game_over = True
+            celebration_message = f"{turn} Won🥳 AAHA TAMATAR BADE MAJEDAAR..."
+            celebration(celebration_message, 0.2)
+            return
+
+        if(board[0][2] == board[1][1] and board[1][1] == board[2][0]):
+            game_over = True
+            celebration_message = f"{turn} Won🥳 AAHA TAMATAR BADE MAJEDAAR..."
+            celebration(celebration_message, 0.2)
+            return
+
+    print_board()
+
+    while(game_over == False):
+        player1_enter_valid_key = False
+        player2_enter_valid_key = False
+        while(player1_enter_valid_key == False):
+            player1 = input(f"{turn}'s turn: ")
+            response = update_board(player1)
+            if(response == "Valid"):
+                player1_enter_valid_key = True
+                print_board()
+            else:
+                print("Entered key is invalid try again.")
+                
+
+        if(game_over == False):
+            while(player2_enter_valid_key == False):
+                player2 = input(f"{turn}'s turn: ")
+                response = update_board(player2)
+                if(response == "Valid"):
+                    player2_enter_valid_key = True
+                    print_board()
+                else:
+                    print("Entered key is invalid try again.")
+
+tic_tac_toe()
+            
+
+
+
         
 
