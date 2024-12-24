58efc1340969a366f8e4898a49ab589b4a31b12d
anurag9601 <anuragmishrap13@gmail.com>
2024-12-24 14:58:53 +0530

    upload 24_dec.py

---
+++ 24_dec.py
@@ -0,0 +1,36 @@
+def alpha_clash(str_A, ind_A, str_Z, ind_Z):
+    filter_str_A = ""
+    filter_str_Z = ""
+
+    str_A_points = 0
+    str_Z_points = 0
+
+    for i in range(len(str_A)):
+        if(i not in ind_Z):
+            filter_str_A += str_A[i]
+    for j in range(len(str_Z)):
+        if(j not in ind_A):
+            filter_str_Z += str_Z[j]
+
+    player_A_asci_val = [ord(x) for x in filter_str_A] #List comprehension
+    player_Z_asci_val = [ord(y) for y in filter_str_Z]
+
+    for i in range(len(player_A_asci_val)):
+        if(player_A_asci_val[i] > player_Z_asci_val[i]):
+            str_A_points += player_A_asci_val[i] - player_Z_asci_val[i]
+        elif(player_Z_asci_val[i] > player_A_asci_val[i]):
+            str_Z_points += player_Z_asci_val[i] - player_A_asci_val[i]
+
+    return { "A" : str_A_points, "Z" : str_Z_points }
+
+# print(alpha_clash("MZNHUVIOEPTWFJCBXKALSDQGYR",
+#   [1, 3, 2, 8, 10, 12, 9, 7, 4, 22],
+#   "YFTUCSQOMGKPXNDWHIVJRABZEL",
+#   [21, 24, 25, 3, 4, 1, 8, 9, 10, 17]))
+
+# print(alpha_clash(
+#   "OZLICHFRKYBVUDSPWXJNGTQAEM",
+#   [8, 6, 4, 2, 0, 10, 12, 14, 16, 18],
+#   "WKJVUNXHRFDIOBTCSLZMPYGQAE",
+#   [7, 5, 3, 1, 9, 11, 13, 15, 17, 19]
+# ))
