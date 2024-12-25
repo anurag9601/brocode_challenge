c3f63f7914a3c6e5cbd24d43973a51aeab9b10f0
anurag9601 <anuragmishrap13@gmail.com>
2024-12-25 14:13:24 +0530

    update 25_dec.py

--- typescript/25_dec.ts
+++ typescript/25_dec.ts
@@ -1,4 +1,4 @@
-function saveBoom(inputLstObj: number[]) {
+function saveBoom(inputLstObj: number[]): string {
     for (let numI: number = 0; numI < inputLstObj.length; numI++) {
         const strInt: string = inputLstObj[numI].toString();
         if (strInt.includes("7")) {
@@ -12,7 +12,7 @@ function saveBoom(inputLstObj: number[]) {
 // console.log(saveBoom([1, 2, 3, 4, 5, 6, 7]));
 // console.log(saveBoom([8, 6, 33, 100]));
 
-function countBoomerangs(inputLst: number[]) {
+function countBoomerangs(inputLst: number[]): number {
     let noOfBoomerangs: number = 0
 
     for (let i = 0; i < inputLst.length; i++) {
@@ -35,3 +35,31 @@ function countBoomerangs(inputLst: number[]) {
 // console.log(countBoomerangs([3, 7, 3, 2, 1, 5, 1, 2, 2, -2, 2]))
 // console.log(countBoomerangs([4, 4, 4, 9, 9, 9, 9]))
 // console.log(countBoomerangs([5, 6, 6, 7, 6, 3, 9]))
+
+function getLength(nestedList: unknown[]): number {
+    let lengthCount: number = 0
+
+    for (let element of nestedList) {
+        if (Array.isArray(element)) {
+            lengthCount += getLength(element)
+        } else {
+            lengthCount++;
+        }
+    }
+
+    return lengthCount
+}
+
+// console.log(getLength([1, [2, [3, [4, [5, 6]]]]]))
+// console.log(getLength([1, [2], 1, [2], 1]))
+
+function invert(inputStr: string): string {
+    if (inputStr == "") {
+        return ""
+    }
+    const currentString: string = inputStr[0].toLowerCase() == inputStr[0] ? inputStr[0].toUpperCase() : inputStr[0].toLowerCase()
+    return invert(inputStr.slice(1)) + currentString
+}
+
+// console.log(invert("dLROW YM sI HsEt"))
+// console.log(invert("ytInIUgAsnOc"))
