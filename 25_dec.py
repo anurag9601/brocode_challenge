b595b0775148a861b1d6e2afe63ba9f68a8b8cf4
anurag9601 <anuragmishrap13@gmail.com>
2024-12-26 14:38:46 +0530

    update 25_dec.py

--- 25_dec.py
+++ 25_dec.py
@@ -13,5 +13,26 @@ def canConstruct(ransomNote, magazine):
     else:
         return False
     
-print(canConstruct("a", "b"))
-print(canConstruct("aab", "baa"))
+# print(canConstruct("a", "b"))
+# print(canConstruct("aab", "baa"))
+
+def simplify(input_str):
+    split_input_str = input_str.split("/")
+    first_int = int(split_input_str[0])
+    second_int = int(split_input_str[1])
+    k = first_int if first_int < second_int else second_int
+
+    result = []
+
+    for i in range(2, k + 1):
+        if(first_int%i == 0 and second_int%i == 0):
+            result = []
+            result.append(str(first_int//i))
+            result.append(str(second_int//i))
+        
+    return input_str if result == [] else "/".join(result)
+
+# print(simplify("4/6"))
+# print(simplify("100/400"))
+# print(simplify("8/4"))
+# print(simplify("10/11"))
