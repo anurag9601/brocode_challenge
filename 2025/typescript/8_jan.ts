dbd7fb0bd83a8901b40327358bc87b8e38e6db2c
anurag9601 <anuragmishrap13@gmail.com>
2025-01-08 16:12:27 +0530

    update 8_jan.ts

--- 2025/typescript/8_jan.ts
+++ 2025/typescript/8_jan.ts
@@ -19,12 +19,35 @@ function sum_number(n: number): number {
 // console.log(sum_number(5))
 
 function multi_sum(n: number, index: number = 1): number {
-    if (index == 10) {
-        return n
+    if (index > 10) {
+        return 0;
     }
 
     return n * index + multi_sum(n, index + 1)
 }
 
-console.log(multi_sum(6))
-console.log(multi_sum(10))
+// console.log(multi_sum(6))
+// console.log(multi_sum(10))
+
+function reverse(input_str:string): string{
+    if(input_str.length == 1 || input_str.length == 0){
+        return input_str;
+    }
+
+    return reverse(input_str.slice(1)) + input_str[0]
+}
+
+// console.log(reverse("hello"))
+
+function fibo(n:number): number{
+    if(n == 0 || n == 1){
+        return n
+    }
+
+    return fibo(n - 2) + fibo(n - 1)
+}
+
+// console.log(fibo(8))
+// console.log(fibo(2))
+// console.log(fibo(1))
+// console.log(fibo(0))
