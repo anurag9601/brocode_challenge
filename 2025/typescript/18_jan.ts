d518f942c0b6c404c4d56918f4853cad743d6438
anurag9601 <anuragmishrap13@gmail.com>
2025-01-18 20:40:15 +0530

    update 18_jan.ts

--- 2025/typescript/18_jan.ts
+++ 2025/typescript/18_jan.ts
@@ -1,4 +1,4 @@
-function rootDigit(n: number, sum: number = 0) {
+function rootDigit(n: number, sum: number = 0): number {
     let lstNum = n % 10
     sum = sum + lstNum
     let strSum = sum.toString()
@@ -14,3 +14,32 @@ function rootDigit(n: number, sum: number = 0) {
 // console.log(rootDigit(999888777))
 // console.log(rootDigit(1238763636555555555555))
 
+function isPalindrome(str: string): boolean {
+    if (str.length <= 1) {
+        return true
+    } else if (str[0] != str[str.length - 1]) {
+        return false
+    }
+    return isPalindrome(str.slice(1, -1))
+}
+
+// console.log(isPalindrome("abcba"))
+// console.log(isPalindrome("b"))
+// console.log(isPalindrome(""))
+// console.log(isPalindrome("ab"))
+
+function bugger(n: number, sum: number = 1, recurCount = 0) {
+    let lstNum = n % 10
+    sum *= lstNum
+    if (n < 10 && sum < 10) {
+        return recurCount > 0 ? recurCount + 1 : recurCount
+    }
+    if (n < 10) {
+        return bugger(sum, 1, recurCount+1)
+    }
+    return bugger(Math.floor(n / 10), sum, recurCount)
+}
+
+// console.log(bugger(39))
+// console.log(bugger(999))
+// console.log(bugger(4))
