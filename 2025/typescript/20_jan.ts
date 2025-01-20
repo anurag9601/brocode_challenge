19aab61c2c06d3e19e2e5f9f4c03e81538467a70
anurag9601 <anuragmishrap13@gmail.com>
2025-01-20 16:47:22 +0530

    update 20_jan.ts

--- 2025/typescript/20_jan.ts
+++ 2025/typescript/20_jan.ts
@@ -32,3 +32,56 @@ function getHashTags(inputStr: string): string[] {
 // console.log(getHashTags("Hey Parents, Surprise, Fruit Juice Is Not Fruit"));
 // console.log(getHashTags("Visualizing Science"));
 
+function cleanUp(fileLst: string[], method: string) {
+    let result: unknown[] = []
+    let doneList: string[] = []
+    let currentIndex = 0
+    if (method == "suffix") {
+        for (let i of fileLst) {
+            let tempLst = []
+            let extension = i.split(".")[1]
+            if (!doneList.includes(extension)) {
+                doneList.push(extension)
+                for (let j of fileLst) {
+                    let currentExtension = j.split(".")[1]
+                    if (currentExtension == extension) {
+                        tempLst.push(j)
+                    }
+                }
+                result.push(tempLst)
+            }
+        }
+    } else {
+        for (let i = 0; i < fileLst.length; i++) {
+            let tempLst = [];
+            let passedFile: string[] = [];
+            if (currentIndex < fileLst.length) {
+                for (let j = currentIndex; j < fileLst.length; j++) {
+                    let extension = fileLst[j].split(".")[1]
+                    if (!passedFile.includes(extension)) {
+                        passedFile.push(extension)
+                        tempLst.push(fileLst[j])
+                        if (j == fileLst.length - 1) {
+                            currentIndex = j + 1
+                            result.push(tempLst);
+                            break
+                        }
+                    } else {
+                        currentIndex = j
+                        result.push(tempLst)
+                        break;
+                    }
+                }
+            } else {
+                break
+            }
+        }
+    }
+
+    return result
+}
+console.log(cleanUp(["ex1.html", "ex1.js", "ex2.html", "ex2.js"], "prefix"));
+console.log(cleanUp(["ex1.html", "ex1.js", "ex2.html", "ex2.js"], "suffix"));
+console.log(cleanUp(["music_app.js", "music_app.png", "music_app.wav", "tetris.png", "tetris.js"], "prefix"));
+console.log(cleanUp(["_1.rb", "_2.rb", "_3.rb", "_4.rb"], "suffix"));
+console.log(cleanUp(["_1.rb", "_2.rb", "_3.rb", "_4.rb"], "prefix"));
