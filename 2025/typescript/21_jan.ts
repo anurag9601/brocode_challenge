09292553d5570f99a8c2e0ac5b762a8554005314
anurag9601 <anuragmishrap13@gmail.com>
2025-01-24 11:06:32 +0530

    update 22_jan.ts

--- 2025/typescript/21_jan.ts
+++ 2025/typescript/21_jan.ts
@@ -40,3 +40,28 @@ function product(numLst: number[], maxOrMin: string) {
 // console.log(product([-8, 1, 2, 7, 9], "max"))
 // console.log(product([1, -1, 1, 1], "min"))
 // console.log(product([-5, -3, -1, 0, 4], "min"))
+
+
+function minLength(numLst: number[], num: number) {
+    let sortestLength: number = Infinity;
+    for (let i = 0; i < numLst.length - 1; i++) {
+        let arrCount = 0;
+        let currentSum = 0;
+        for (let j = i; j < numLst.length; j++) {
+            currentSum += numLst[j]
+            arrCount++;
+            if (currentSum > num) {
+                if (arrCount < sortestLength) {
+                    sortestLength = arrCount
+                }
+                break;
+            }
+        }
+    }
+    return sortestLength === Infinity ? -1 : sortestLength
+}
+
+// console.log(minLength([5, 8, 2, -1, 3, 4], 9));
+// console.log(minLength([3, -1, 4, -2, -7, 2], 4));
+// console.log(minLength([1, 0, 0, 0, 1], 1));
+// console.log(minLength([0, 1, 1, 0], 2));
---
+++ 2025/typescript/22_jan.ts
@@ -0,0 +1,34 @@
+function caesarCipher(inputStr: string, jump: number): string {
+    let lstInputStr = inputStr.split("")
+    let alphabet: string = "abcdefghijklmnopqrstuvwxyz";
+    for (let i = 0; i < lstInputStr.length; i++) {
+        if (alphabet.includes(lstInputStr[i].toLocaleLowerCase())) {
+            let isUpper = false;
+            let currentLetterIndex = alphabet.indexOf(lstInputStr[i].toLocaleLowerCase());
+            if (lstInputStr[i] != alphabet[currentLetterIndex]) {
+                isUpper = true;
+            }
+            currentLetterIndex += jump
+            if (currentLetterIndex > alphabet.length) {
+                let remainIndex = currentLetterIndex - (alphabet.length);
+                if (isUpper == true) {
+                    lstInputStr[i] = alphabet[remainIndex].toUpperCase();
+                } else {
+                    lstInputStr[i] = alphabet[remainIndex]
+                }
+            } else {
+                if (isUpper == true) {
+                    lstInputStr[i] = alphabet[currentLetterIndex].toUpperCase();
+                } else {
+                    lstInputStr[i] = alphabet[currentLetterIndex]
+                }
+            }
+        }
+    }
+
+    return lstInputStr.join("")
+}
+
+// console.log(caesarCipher("middle-Outz", 2));
+// console.log(caesarCipher("Always-Look-on-the-Bright-Side-of-Life", 5));
+// console.log(caesarCipher("A friend in need is a friend indeed", 20));
