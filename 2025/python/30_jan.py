8decabb51c808e0289698ac0b4be2e156e0941d8
anurag9601 <anuragmishrap13@gmail.com>
2025-01-31 10:56:03 +0530

    update 30_jan.py

--- 2025/python/30_jan.py
+++ 2025/python/30_jan.py
@@ -15,3 +15,32 @@ def atbash(input_str):
     
 # print(atbash("apple"))
 # print(atbash("Christmas is the 25th of December"))
+
+
+def dif_ciph(input_str):
+    def custom_ord(char):
+        if( not isinstance(char, str) or len(char) != 1):
+            raise TypeError("not a string or an empty string")
+    
+        result = 0
+        for i in range(len(char)):
+            result = (result << 8) + (lambda c: c.encode("utf-8")[0])(char)
+
+        return result
+    
+    prev = 0
+    encoded_num = []
+
+    for char in input_str:
+        current = custom_ord(char)
+        if(prev == 0):
+            encoded_num.append(current)
+            prev = current
+        else:
+            encoded_num.append(current - prev)
+            prev = current
+
+    return encoded_num
+
+# print(dif_ciph("Hello"))
+# print(dif_ciph("Sunshine"))
