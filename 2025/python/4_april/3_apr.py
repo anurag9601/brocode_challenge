84b26ee561357de08a309888be3d8b9159c55029
anurag9601 <anuragmishrap13@gmail.com>
2025-04-03 22:17:23 +0530

    upload 3_apr.py

---
+++ 2025/python/4_april/3_apr.py
@@ -0,0 +1,77 @@
+# for i in range(5):
+#     for j in range(9-i):
+#         if j >= i:
+#             if i%2 == 0:
+#                 if j%2 == 0:
+#                     print(5-i, end="")
+#                 else:
+#                     print(" ", end="")
+#             elif i%2 != 0:
+#                 if j%2 != 0:
+#                     print(5-i, end="")
+#                 else:
+#                     print(" ", end="")
+#         else:
+#             print(" ", end="")
+#     print()
+
+# 5 5 5 5 5
+#  4 4 4 4
+#   3 3 3
+#    2 2
+#     1
+
+# for i in range(5):
+#     print_state = 5-i
+#     for j in range(9-i):
+#         if j >= i:
+#             if i%2 == 0:
+#                 if j%2 == 0:
+#                     print(print_state, end="")
+#                     print_state -= 1
+#                 else:
+#                     print(" ", end="")
+#             elif i%2 != 0:
+#                 if j%2 != 0:
+#                     print(print_state, end="")
+#                     print_state -= 1
+#                 else:
+#                     print(" ", end="")
+#         else:
+#             print(" ", end="")
+#     print()
+
+# 5 4 3 2 1
+#  4 3 2 1
+#   3 2 1
+#    2 1
+#     1
+
+# for i in range(9):
+#     iterate = 5+i if i < 5 else iterate - 1
+#     print_state = iterate - 4
+#     for j in range(iterate):
+#         if j >= abs(5-(i+1)):
+#             if i%2 == 0:
+#                 if j%2 == 0:
+#                     print(print_state, end="")
+#                 else:
+#                     print(" ", end="")
+#             elif i%2 != 0:
+#                 if j%2 != 0:
+#                     print(print_state, end="")
+#                 else:
+#                     print(" ", end="")
+#         else:
+#             print(" ", end="")
+#     print()
+
+#     1
+#    2 2
+#   3 3 3
+#  4 4 4 4
+# 5 5 5 5 5
+#  4 4 4 4
+#   3 3 3
+#    2 2
+#     1
