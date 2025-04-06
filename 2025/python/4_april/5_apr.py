8d00281846551dd19f5a5320db3aae7baf37ad4e
anurag9601 <anuragmishrap13@gmail.com>
2025-04-06 20:00:36 +0530

    update 5_apr.py

--- 2025/python/4_april/5_apr.py
+++ 2025/python/4_april/5_apr.py
@@ -1,1 +1,24 @@
-#Tomorrow I will solve the problems here. 
+def find_median_sorted_array(nums1, nums2):
+    merge = []
+
+    i = j = 0
+
+    while i < len(nums1) and j < len(nums2):
+        if nums1[i] < nums2[j]:
+            merge.append(nums1[i])
+            i += 1
+        else:
+            merge.append(nums2[j])
+            j += 1
+
+    merge.extend(nums1[i:])
+    merge.extend(nums2[j:])
+    
+    merge_l = len(merge)
+
+    if merge_l % 2 == 0:
+        return (merge[merge_l//2 - 1] + merge[merge_l//2]) / 2
+    else:
+        return merge[merge_l//2]
+
+# print(find_median_sorted_array([1,2],[3,4]))
