2423f182e6e93facf0444cebfae0b891a546fbd5
anurag9601 <anuragmishrap13@gmail.com>
2025-04-08 21:58:45 +0530

    update 7_apr.py

--- 2025/python/4_april/7_apr.py
+++ 2025/python/4_april/7_apr.py
@@ -17,4 +17,47 @@ def convert(s, numRows):
                 res += s[i + increment - 2 * r]
     return res
 
-print(convert("PAYPALISHIRING", 3))
+# print(convert("PAYPALISHIRING", 3))
+
+# 2. Add Two Numbers ListNode Problem
+
+def addTwoNumbers(l1, l2):
+    dummy = ListNode()
+    cur = dummy
+
+    carry = 0
+    while l1 or l2 or carry:
+        v1 = l1.val if l1 else 0
+        v2 = l2.val if l2 else 0
+
+        val = v1 + v2 + carry
+
+        carry = val // 10
+        val = val % 10
+        cur.next = ListNode(val)
+
+        cur = cur.next
+        l1 = l1.next if l1 else None
+        l2 = l2.next if l2 else None
+
+    return dummy.next
+
+# 11. Container With Most Water
+# Solving using two pointer approach
+
+def maxArea(heights):
+    lp, rp , ans = 0, len(heights) - 1, 0
+
+    while lp < rp:
+        w = rp - lp
+        h = min(heights[lp], heights[rp])
+        cur_ans = w * h
+        ans = max(ans, cur_ans)
+        if heights[lp] > heights[rp]:
+            rp -= 1
+        else:
+            lp += 1
+
+    return ans
+
+# print(maxArea([1,8,6,2,5,4,8,3,7]))
