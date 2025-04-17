a6be9061a1fef074fb14de2f0fada62192af8bd8
anurag9601 <anuragmishrap13@gmail.com>
2025-04-17 23:56:03 +0530

    update 17_apr.py

--- 2025/python/4_april/17_apr.py
+++ 2025/python/4_april/17_apr.py
@@ -90,3 +90,110 @@ def stair(n):
 # print(stair(3))
 # print(stair(4))
 # print(stair(10))
+
+# Python has built-in string validation methods for basic data. It can check if a string is composed of alphabetical characters, alphanumeric characters, digits, etc.
+
+# str.isalnum()
+# This method checks if all the characters of a string are alphanumeric (a-z, A-Z and 0-9).
+
+# if __name__ == '__main__':
+    # s = input()
+    
+    # isAlphaNumeric = False
+    # isHasAnyAlpha = False
+    # isHasAnyDigit = False
+    # isHasAnyLower = False
+    # isHasAnyUpper = False
+    
+    # for char in s:
+    #     if char.isalnum():
+    #         isAlphaNumeric = True
+    #     if char.isalpha():
+    #         isHasAnyAlpha = True
+    #     if char.isdigit():
+    #         isHasAnyDigit = True
+    #     if char.islower():
+    #         isHasAnyLower = True
+    #     if char.isupper():
+    #         isHasAnyUpper = True
+        
+    # print(isAlphaNumeric)
+    # print(isHasAnyAlpha)
+    # print(isHasAnyDigit)
+    # print(isHasAnyLower)
+    # print(isHasAnyUpper)
+
+# 16. 3Sum Closest
+# Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+# Return the sum of the three integers.
+# You may assume that each input would have exactly one solution.
+# Example 1:
+# Input: nums = [-1,2,1,-4], target = 1
+# Output: 2
+# Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+# Example 2:
+
+# Input: nums = [0,0,0], target = 1
+# Output: 0
+# Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+
+def threeSumClosest(self, nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    close_sum = 0
+    difference = float("inf")
+    nums.sort()
+
+    for i,a in enumerate(nums):
+        if i > 0 and a == nums[i - 1]:
+            continue
+        l , r = i + 1, len(nums) - 1
+        while l < r:
+            threeSum = a + nums[l] + nums[r]
+            if abs(threeSum - target) < difference:
+                close_sum = threeSum
+                difference = abs(threeSum - target)
+            if threeSum < target:
+                l += 1
+            elif threeSum > target:
+                r -= 1
+            else:
+                l += 1
+    return close_sum
+
+# Create a function that takes a string and returns the reversed string. However there's a few rules to follow in order to make the challenge interesting:
+# The UPPERCASE/lowercase positions must be kept in the same order as the original string (see example #1 and #2).
+# Spaces must be kept in the same order as the original string (see example #3).
+# Examples
+# special_reverse_string("Edabit") ➞ "Tibade"
+# special_reverse_string("UPPER lower") ➞ "REWOL reppu"
+# special_reverse_string("1 23 456") ➞ "6 54 321"
+
+def special_reverse_string(s):
+    result = ""
+    reverse_char = "".join("".join(list(reversed(s))).split(" "))
+    index = 0
+    
+    for char in s:
+        if char == " ":
+            result += " "
+        elif char.isupper():
+            result += reverse_char[index].upper()
+            index += 1
+        elif char.islower():
+            result += reverse_char[index].lower()
+            index += 1
+        else:
+            result += reverse_char[index]
+            index += 1
+    
+    return result
+
+# print(special_reverse_string("UPPER lower"))
+# print(special_reverse_string("Edabit"))
+# print(special_reverse_string("1 23 456"))
+
+
