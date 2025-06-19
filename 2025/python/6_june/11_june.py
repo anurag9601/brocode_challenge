552c7063749fa74f3379818b831d31538eee0090
anurag9601 <anuragmishrap13@gmail.com>
2025-06-19 23:42:15 +0530

    created 19_june.py

--- 2025/python/6_june/11_june.py
+++ 2025/python/6_june/11_june.py
@@ -30,3 +30,35 @@ def valid_word_nest(word, nested_word):
 # print(valid_word_nest("novel", "nonnonovnovnovelelelvelovelvel"))
 # print(valid_word_nest("painter", "ppaintppapaipainterinternteraintererainter"))
 # print(valid_word_nest("shape", "sssshapeshapehahapehpeape"))
+
+def letterCombinations(digits):
+    digit_to_char = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+    
+    if digits == "":
+        return []
+    
+    result = []
+
+    def backtrack(i,curr_str):
+        if len(curr_str) == len(digits):
+            result.append(curr_str)
+            return
+        
+        for char in digit_to_char[digits[i]]:
+            backtrack(i+1, curr_str + char)
+    
+    backtrack(0, "")
+
+    return result
+
+# print(letterCombinations("23"))
+# print(letterCombinations("569"))
