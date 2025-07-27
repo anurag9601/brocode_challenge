a73e60286ef7b14d2b3868ce8d80166fe8ec75ce
anurag9601 <anuragmishrap13@gmail.com>
2025-07-27 17:18:18 +0530

    created 10_july.py

---
+++ 2025/python/7_july/10_july.py
@@ -0,0 +1,11 @@
+def hangman(string, input_list):
+    result = ""
+    for i in string:
+        if i in input_list or i == " ":
+            result += i
+        else:
+            result += "-"
+    return result
+
+print(hangman("helicopter", ["o", "e", "s"]))
+print(hangman("tree", ["r", "t", "e"]))
