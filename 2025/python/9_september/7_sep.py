957bff3e101d555e5820d79ccf6b1815eb5d5d1f
anurag9601 <anuragmishrap13@gmail.com>
2025-10-02 17:07:34 +0530

    updated 7_sep.py

--- 2025/python/9_september/7_sep.py
+++ 2025/python/9_september/7_sep.py
@@ -20,3 +20,32 @@ def traffic(present_cars, car_pass_limit, signal_green_count):
 # print(traffic(5, 2, 2))
 # print(traffic(3, 5, 1))
 # print(traffic(0, 0, 0))
+
+# Flight Boarding — Passenger Groups (Hard)
+# At an airport, passengers board in groups. Each group must be seated together, and each row has 6 seats.
+# Create a function that assigns groups to rows.
+
+def boarding(passengers_list):
+    seat_limit = 6
+    seating_arragements = []
+
+    curr_row = []
+    seated_pass = 0
+    for passenger in passengers_list:
+        curr_pass_count = seated_pass + passenger
+        if curr_pass_count > seat_limit:
+            seating_arragements.append(curr_row)
+            seated_pass = 0
+            curr_row = []
+
+            seated_pass += passenger
+            curr_row.append(passenger)
+        else:
+            seated_pass += passenger
+            curr_row.append(passenger)
+    if len(curr_row) > 0:
+        seating_arragements.append(curr_row)
+    return seating_arragements
+
+# print(boarding([6, 2, 2, 2, 5]))
+# print(boarding([2, 4, 3, 5]))
