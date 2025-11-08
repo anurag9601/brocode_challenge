ff21774c8060b36ad88a365049ea529482abe6cc
anurag9601 <anuragmishrap13@gmail.com>
2025-11-08 22:39:56 +0530

    upload 10_oct.py

---
+++ 2025/python/10_october/10_oct.py
@@ -0,0 +1,14 @@
+def freq_count(num_list, ele, sub_list_count=0):
+    present_ele_count = 0
+
+    for i in range(len(num_list)):
+        if isinstance(num_list[i], list):
+            return freq_count(num_list[i], ele, sub_list_count + 1)
+        elif isinstance(num_list[i], int) and num_list[i] == ele:
+            present_ele_count += 1
+    return [sub_list_count, present_ele_count]
+
+print(freq_count([1, 4, 4, [1, 1, [1, 2, 1, 1]]], 1))
+print(freq_count([1, 5, 5, [5, [1, 2, 1, 1], 5, 5], 5, [5]], 5))
+print(freq_count([1, [2], 1, [[2]], 1, [[[2]]], 1, [[[[2]]]]], 2))
+
