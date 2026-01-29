5c81245deee078f993272b6d15f590cfaec2d500
Anurag Mishra <anuragmishrap13@gmail.com>
2026-01-29 12:21:35 +0530

    solved more pattern problems

--- 2026/python/1_january/29_jan.py
+++ 2026/python/1_january/29_jan.py
@@ -1,16 +1,46 @@
 alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-space_c = 4
-for i in range(1, 10, 2):
-    for j in range(space_c):
-        print(" ", end="")
-    for k in range(i):
-        print(alpha[i - 1], end="")
-    space_c -= 1
-    print()
+# space_c = 4
+# for i in range(1, 10, 2):
+#     for j in range(space_c):
+#         print(" ", end="")
+#     for k in range(i):
+#         print(alpha[i - 1], end="")
+#     space_c -= 1
+#     print()
 
 #     A
 #    CCC
 #   EEEEE
 #  GGGGGGG
 # IIIIIIIII
+
+# space_c = 4
+# for i in range(1, 10, 2):
+#     for j in range(space_c):
+#         print(" ", end="")
+#     for k in range(i):
+#         print(alpha[k], end="")
+#     space_c -= 1
+#     print()
+
+#     A
+#    ABC
+#   ABCDE
+#  ABCDEFG
+# ABCDEFGHI
+
+# space_c = 4
+# for i in range(1, 10, 2):
+#     for j in range(space_c):
+#         print(end=" ")
+#     for k in range(i- 1 , -1, -1):
+#         print(alpha[k], end="")
+#     space_c -= 1
+#     print()
+
+#     A
+#    CBA
+#   EDCBA
+#  GFEDCBA
+# IHGFEDCBA
