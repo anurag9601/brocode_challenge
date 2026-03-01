51b6ca475a53b12e9157424371d092faf07ea0a5
anurag9601 <anuragmishrap13@gmail.com>
2026-03-01 21:57:01 +0530

    updated standard sorting

--- 2026/python/2_february/24_feb.py
+++ 2026/python/2_february/24_feb.py
@@ -1,8 +1,8 @@
-#shorting DSA algorithoms
+#sorting DSA algorithoms
 
-#bubble shorting
+#bubble sorting
 
-class bubble_short:
+class bubble_sort:
     def __init__(self, num_list):
         self.num_list = num_list
         self.list_length = len(num_list)
@@ -14,5 +14,25 @@ class bubble_short:
                     self.num_list[j],self.num_list[j + 1] = self.num_list[j + 1], self.num_list[j]
         return self.num_list
     
-result_bubble_short = bubble_short([64, 34, 25, 12, 22, 11, 90, 5])()
-print(result_bubble_short)
+# result_bubble_sort = bubble_sort([64, 34, 25, 12, 22, 11, 90, 5])()
+# print(result_bubble_sort)
+
+#selection sorting
+
+class selection_sort:
+    def __init__(self, num_list):
+        self.num_list = num_list
+        self.list_length = len(num_list)
+        
+    def __call__(self):
+        for i in range(self.list_length):
+            min_i = i
+            for j in range(i, self.list_length):
+                if self.num_list[j] < self.num_list[min_i]:
+                    min_i = j
+            self.num_list[min_i],self.num_list[i] = self.num_list[i],self.num_list[min_i]
+        return self.num_list
+    
+# result_selection_sort = selection_sort([64, 34, 25, 5, 22, 11, 90, 12])()
+# print(result_selection_sort)
+                
